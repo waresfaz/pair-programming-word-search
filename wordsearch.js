fb68677ecbb0cda9c4d99661a43287f488846f2a
Wares <fazelyar.wares@gmail.com>
2020-04-08 21:58:40 +0000

    completed function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
 const wordSearch = (letters, word) => { 
+    const rows = letters[0].length;
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    
+    for (let col = 0; col < rows; col++) {
+        let column = letters.map((value) => { 
+            return cvalue[col];
+        });
+       const verticalJoin = column.join("");
+       if (verticalJoin.includes(word)) {
+           return true;
+       }
+       
     }
+    for (l of horizontalJoin) {
+        
+        if (l.includes(word)) {
+            return true; 
+             
+        }
+    }
+    return false;
 }
+
+// pair programmed with Jacob
+
+console.log(wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'FRANK'));
+
 
 module.exports = wordSearch
